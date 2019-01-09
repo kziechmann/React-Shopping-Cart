a8c030f4bc3e04ce381491d5b855a0cb74c8c21b
Kevin <kziechmann@gmail.com>
2019-01-09 16:35:46 -0700

    Created basic React app, replaced default styling with Bootstrap and made footer and navbar components.

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">@</a>
+      </nav>
+      );
+  }
+}
+
+export default Footer;
